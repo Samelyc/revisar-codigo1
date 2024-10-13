b0047658434ab1b46617c497b2b2ee29347496b2
Elisa Cruz <elisamcg3@gmail.com>
2024-10-13 00:18:30 -0600

    feat: se agregaron los cambios y se hicieron comentarios, tambien se cambio la conexion remota por mi repo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,29 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $name = document.querySelector('.name'); // se completo el nombre $name Esta definidos como clases, se agrego un punto
+const $blog = document.querySelector('.blog');// se completo el nombre $blog Esta definidos como clases, se agrego un punto
+const $location = document.querySelector('.location');// se completo el nombre $location Esta definidos como clases, se agrego un punto y se agrego la case en el html
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+
+
+async function displayUser (username) {
+  $name.textContent = 'cargando...';
+
+  let response = await fetch (`${usersEndpoint}/${username}`); 
+ 
+  let data = await response.json(); // se cambio la respuesta a .json
+  
+  
+  $name.textContent = (`${data.name}`); // Las comillas estan mal colocadas se cambiaron por las correctas
+  $blog.textContent = (`${data.blog}`);// Las comillas estan mal colocadas se cambiaron por las correctas
+  $location.textContent = (`${data.location}`);// Las comillas estan mal colocadas se cambiaron por las correctas
 }
 
 function handleError(err) {
-  console.log('OH NO!');
+  console.log("OH NO!");
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $name.textContent = `Algo salió mal: ${err}`//n no esta definida, pero "$nombre" si.
 }
 
 displayUser('stolinski').catch(handleError);
+
